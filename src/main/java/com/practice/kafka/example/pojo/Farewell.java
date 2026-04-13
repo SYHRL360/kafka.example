a2280f2148436ff5e32747a053ef5320bcb3d352
Syahrul Ramadhan <syahrul.ramadhan@pinjamango.co.id>
2026-04-13 17:53:29 +0700

    Adding object class for kafka message

--- src/main/java/com/practice/kafka/example/pojo/Farewell.java
+++ src/main/java/com/practice/kafka/example/pojo/Farewell.java
@@ -1,2 +1,16 @@
-package com.practice.kafka.example.pojo;public class Farewell {
+package com.practice.kafka.example.pojo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class Farewell {
+
+    private String message;
+    private Integer remainingMinutes;
+
+
 }
--- src/main/java/com/practice/kafka/example/pojo/Greeting.java
+++ src/main/java/com/practice/kafka/example/pojo/Greeting.java
@@ -1,2 +1,18 @@
-package com.practice.kafka.example.pojo;public class Greeting {
+package com.practice.kafka.example.pojo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class Greeting {
+
+    private String msg;
+    private String name;
 }
+
+
+
+
