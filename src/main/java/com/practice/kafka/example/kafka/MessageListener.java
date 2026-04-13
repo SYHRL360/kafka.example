806b663642a17a3ab375b140fc08782d280fc8aa
Syahrul Ramadhan <syahrul.ramadhan@pinjamango.co.id>
2026-04-13 17:55:22 +0700

    Add kafka message producer and listener

--- src/main/java/com/practice/kafka/example/kafka/MessageListener.java
+++ src/main/java/com/practice/kafka/example/kafka/MessageListener.java
@@ -1,2 +1,39 @@
-package com.practice.kafka.example.kafka;public class MessageListener {
+package com.practice.kafka.example.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+public class MessageListener {
+
+    public CountDownLatch latch = new CountDownLatch(3);
+
+    public CountDownLatch partitionLatch = new CountDownLatch(2);
+
+    public CountDownLatch filterLatch = new CountDownLatch(2);
+
+    public CountDownLatch greetingLatch = new CountDownLatch(1);
+
+    @KafkaListener(topics = "${message.topic.name}", groupId = "foo", containerFactory = "fooKafkaListenerContainerFactory")
+    public void listenGroupFoo(String message) {
+        System.out.println("Received Message in group 'foo': " + message);
+        latch.countDown();
+    }
+
+    @KafkaListener(topics = "${message.topic.name}", groupId = "bar", containerFactory = "barKafkaListenerContainerFactory")
+    public void listenGroupBar(String message) {
+        System.out.println("Received Message in group 'bar': " + message);
+        latch.countDown();
+    }
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/practice/kafka/example/kafka/MessageProducer.java
+++ src/main/java/com/practice/kafka/example/kafka/MessageProducer.java
@@ -1,2 +1,63 @@
-package com.practice.kafka.example.kafka;public class MessageProducer {
+package com.practice.kafka.example.kafka;
+
+import com.practice.kafka.example.pojo.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class MessageProducer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, Greeting> greetingKafkaTemplate;
+
+    @Value(value = "${message.topic.name}")
+    private String topicName;
+
+    @Value(value = "${partitioned.topic.name}")
+    private String partitionedTopicName;
+
+    @Value(value = "${filtered.topic.name}")
+    private String filteredTopicName;
+
+    @Value(value = "${greeting.topic.name}")
+    private String greetingTopicName;
+
+    public void sendMessage(String message) {
+
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
+        future.whenComplete((result, exception) -> {
+            if (exception == null) {
+                System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" + message + "] due to : " + exception.getMessage());
+            }
+        });
+    }
+
+    public void sendMessageToPartition(String message, int partition) {
+        kafkaTemplate.send(partitionedTopicName, partition, null, message);
+    }
+
+    public void sendGreetingMessage(Greeting greeting) {
+        greetingKafkaTemplate.send(greetingTopicName, greeting);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
