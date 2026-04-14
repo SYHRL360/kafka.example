67d00124313fb4f5b570744588f3d570318d4394
Syahrul Ramadhan <syahrul.ramadhan@pinjamango.co.id>
2026-04-14 15:14:19 +0700

    Adding lombok annotation

--- src/main/java/com/practice/kafka/example/pojo/Farewell.java
+++ src/main/java/com/practice/kafka/example/pojo/Farewell.java
@@ -1,11 +1,12 @@
 package com.practice.kafka.example.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class Farewell {
 
--- src/main/java/com/practice/kafka/example/pojo/Greeting.java
+++ src/main/java/com/practice/kafka/example/pojo/Greeting.java
@@ -1,11 +1,11 @@
 package com.practice.kafka.example.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class Greeting {
 
